37c2f8b459c2332383fbed7fe3dcd1d8fc115557
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-05-19 14:26:00 +0100

    modified

---
+++ src/js/2-form.js
@@ -0,0 +1,11 @@
+  const formData = {
+    email: "",
+    message: ""
+  };
+
+  const form = document.querySelector(".feedback-form");
+  const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener("input", event => {
+    const { name, value } = event.target;
+})
