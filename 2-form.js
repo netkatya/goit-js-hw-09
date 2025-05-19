7c4a860814d03ce26c8923c0b884e009d5377e69
netkatya <e.klimochenkova@gmail.com>
2025-05-19 13:26:21 +0000

    Deploying to gh-pages from @ netkatya/goit-js-hw-09@37c2f8b459c2332383fbed7fe3dcd1d8fc115557 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const t=document.querySelector(".feedback-form");t.addEventListener("input",e=>{e.target});
+//# sourceMappingURL=2-form.js.map
